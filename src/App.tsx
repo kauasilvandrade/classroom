34d5a0d8d7f877b013fba4ccbf00029aca7dc9ee
Kauã Andrade <kauasilvaz@hotmail.com>
2026-03-10 16:58:42 -0300

    Primeiro projeto em react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/button"
+
+export function App(){
+    return (
+        <div>
+            <Button name="Criar" onClick={() => alert("Criar!")} />
+            <Button name="Editar" />
+            <Button name="Remover" />
+        </div>
+    )
+}
---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+
+
+type Props = React.ComponentProps<"button"> & {
+    name: string
+}
+
+export  function Button({ name, ...rest }: Props) {
+    return <button {...rest}>{name}</button>
+}
