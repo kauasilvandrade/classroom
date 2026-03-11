44d7600619cb5c6cff9673c9d924240a0b0231ce
Kauã Andrade <kauasilvaz@hotmail.com>
2026-03-11 10:29:08 -0300

    css e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import "./global.css"
+import { useState, useEffect } from "react"
+
 import { Button } from "./components/button"
+// import { useMessage } from "./hooks/useMessage"
+
+import styles from "./app.module.css"
 
 export function App(){
+    const [count, setCount] = useState(0) // "0" valor inicial
+    // const message = useMessage({ name: "Kauã", age: 19 })
+
+    function handleAdd() {
+        setCount((prevState) => prevState + 1)
+    }
+
+    function handleRemove() {
+        if (count > 0) {
+            setCount((prevState) => prevState - 1)
+        }
+    }
+
+    useEffect(() => {
+        if (count > 0) {
+            console.log(`O valor mudou para ${count}`)
+        }
+
+    }, [count]) 
     return (
-        <div>
-            <Button name="Criar" onClick={() => alert("Criar!")} />
-            <Button name="Editar" />
-            <Button name="Remover" />
+        <div className={styles.container}>
+            <Button name="Adicionar" onClick={handleAdd} />  
+            <span>{count}</span>
+            <Button name="Remover" onClick={handleRemove} />
         </div>
     )
 }
